aa48a5d0a1d8ec6860a60d8687394e20200acc60
Till Klampaeckel <till@php.net>
2010-09-23 19:49:11 +0200

    * created a lightweight test bootstrap (TestInit.php)  * updated phpunit.xml to use it

---
+++ Tests/TestInit.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * A driver for the PHP OpenID unit tests.
+ *
+ * PHP version 5
+ *
+ * LICENSE: See the COPYING file included in this distribution.
+ *
+ * @package OpenID
+ * @author  Till Klampaeckel <till@php.net>
+ * @license http://www.apache.org/licenses/LICENSE-2.0 Apache
+ */
+
+// necessary when run in the github checkout
+set_include_path(dirname(__FILE__) . '/../:' . get_include_path());
